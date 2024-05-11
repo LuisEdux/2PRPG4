b9a93b4297bc6d8b3f3e96d3d299d20e895ca83f
LuisEdux <a23310141@ceti.mx>
2024-05-10 19:35:36 -0600

    https://github.com/LuisEdux/2PRPG5.git

---
+++ Files/FileHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by barra on 09/05/2024.
+//
+
+#include "FileHandler.h"
---
+++ Files/FileHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by barra on 09/05/2024.
+//
+
+#ifndef RPG_FILEHANDLER_H
+#define RPG_FILEHANDLER_H
+
+
+class FileHandler {
+
+};
+
+
+#endif //RPG_FILEHANDLER_H
